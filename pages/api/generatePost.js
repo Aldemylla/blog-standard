a42839473b03d4491386d312796c42a4280ecf08
aldemylla <aldemylla@gmail.com>
2023-03-24 11:52:58 -0300

    update: The model Davinci-003 to GPT-3.5 for better performance

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -8,22 +8,31 @@ export default async function handler(req, res) {
 
   const { topic, keywords } = req.body;
 
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
     temperature: 0,
     max_tokens: 3600,
-    prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}.
-    The content should be formatted SEO-friendly HTML.
-    The response must also include appropriate HTML title and meta description content.
-    The return format must be stringified JSON in the following format:
-    {
-      "postContent": post content here,
-      "postTitle": post title here,
-      "postMetaDescription": post meta description content here,
-    }`,
+    messages: [
+      {
+        role: "system",
+        content: "You are a blog post generator",
+      },
+      {
+        role: "user",
+        content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}.
+        The content should be formatted SEO-friendly HTML.
+        The response must also include appropriate HTML title and meta description content.
+        The return format must be stringified JSON in the following format:
+        {
+          "postContent": post content here,
+          "postTitle": post title here,
+          "postMetaDescription": post meta description content here,
+        }`,
+      },
+    ],
   });
 
   res.status(200).json({
-    post: JSON.parse(response.data.choices[0]?.text.split("\n").join("")),
+    post: JSON.parse(response.data.choices[0]?.message.content.split("\n").join("")),
   });
 }
